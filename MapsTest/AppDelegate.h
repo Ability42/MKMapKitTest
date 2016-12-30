9cf87e98826afb047119c4c3e1c7dd47019476f9
Stepan Paholyk <stepanpaholyk@gmail.com>
2016-12-30 21:07:53 +0200

    init

---
+++ MapsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapsTest
+//
+//  Created by Stepan Paholyk on 12/30/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapsTest
+//
+//  Created by Stepan Paholyk on 12/30/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
